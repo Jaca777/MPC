3f30a608a42fe80ec5e6908f6126ffb1e700ee7f
JacaDev <szymonrodant@gmail.com>
2014-07-21 14:33:19 +0200

    Up

--- src/tracer/Tracer.java
+++ src/tracer/Tracer.java
@@ -5,7 +5,6 @@ package tracer;
  */
 public class Tracer {
 
-
     public static void varChanged(Object value, String var, String method){
         System.out.println(method + " " + var + " = " + value);
     }
